76509d0c4e0460bf46aaabca8a1f2a4e0af794c6
Marvin Buss <marvin.buss@gmail.com>
2024-10-16 14:17:38 +0200

    lint

--- code/infra/aiservice.tf
+++ code/infra/aiservice.tf
@@ -5,23 +5,23 @@ module "azure_ai_generic" {
     time    = time
   }
 
-  location                                                = var.location_aiservice
-  location_private_endpoint                               = var.location
-  resource_group_name                                     = azurerm_resource_group.resource_group.name
-  tags                                                    = var.tags
-  cognitive_account_name                                  = "${local.prefix}-aig001"
-  cognitive_account_kind                                  = "CognitiveServices"
-  cognitive_account_sku                                   = "S0"
-  cognitive_account_firewall_bypass_azure_services        = true
-  cognitive_account_outbound_network_access_restricted    = true
+  location                                             = var.location_aiservice
+  location_private_endpoint                            = var.location
+  resource_group_name                                  = azurerm_resource_group.resource_group.name
+  tags                                                 = var.tags
+  cognitive_account_name                               = "${local.prefix}-aig001"
+  cognitive_account_kind                               = "CognitiveServices"
+  cognitive_account_sku                                = "S0"
+  cognitive_account_firewall_bypass_azure_services     = true
+  cognitive_account_outbound_network_access_restricted = true
   cognitive_account_outbound_network_access_allowed_fqdns = [
     trimsuffix(trimprefix(module.storage_account.storage_account_primary_blob_endpoint, "https://"), "/"),
     trimsuffix(trimprefix(module.azure_open_ai.cognitive_account_endpoint, "https://"), "/"),
   ]
-  cognitive_account_deployments                           = {}
-  diagnostics_configurations                              = local.diagnostics_configurations
-  subnet_id                                               = azapi_resource.subnet_private_endpoints.id
-  connectivity_delay_in_seconds                           = 0
-  private_dns_zone_id_cognitive_account                   = var.private_dns_zone_id_cognitive_services
-  customer_managed_key                                    = local.customer_managed_key
+  cognitive_account_deployments         = {}
+  diagnostics_configurations            = local.diagnostics_configurations
+  subnet_id                             = azapi_resource.subnet_private_endpoints.id
+  connectivity_delay_in_seconds         = 0
+  private_dns_zone_id_cognitive_account = var.private_dns_zone_id_cognitive_services
+  customer_managed_key                  = local.customer_managed_key
 }
