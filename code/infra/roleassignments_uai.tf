ff6beb7661174be55056137ff5db3c2bc2ac9393
Marvin Buss <marvin.buss@gmail.com>
2024-10-22 09:55:52 +0200

    Add role assignments for blob trigger

--- code/infra/roleassignments_uai.tf
+++ code/infra/roleassignments_uai.tf
@@ -22,10 +22,26 @@ resource "azurerm_role_assignment" "uai_roleassignment_key_vault_secrets_officer
   principal_type       = "ServicePrincipal"
 }
 
-resource "azurerm_role_assignment" "uai_roleassignment_storage_blob_data_contributor" {
-  description          = "Required for accessing the storage account from the ai service and read/write data."
+resource "azurerm_role_assignment" "uai_roleassignment_storage_blob_data_owner" {
+  description          = "Required for accessing the storage account for the blob trigger."
   scope                = module.storage_account.storage_account_id
-  role_definition_name = "Storage Blob Data Contributor"
+  role_definition_name = "Storage Blob Data Owner"
   principal_id         = module.user_assigned_identity.user_assigned_identity_principal_id
   principal_type       = "ServicePrincipal"
 }
+
+resource "azurerm_role_assignment" "uai_roleassignment_storage_queue_data_contributor" {
+  description          = "Required for accessing the storage account for the blob trigger."
+  scope                = module.storage_account.storage_account_id
+  role_definition_name = "Storage Queue Data Contributor"
+  principal_id         = module.user_assigned_identity.user_assigned_identity_principal_id
+  principal_type       = "ServicePrincipal"
+}
+
+# resource "azurerm_role_assignment" "uai_roleassignment_storage_blob_data_contributor" { # Will be ignored, since storage blob data owner permissions have already been granted.
+#   description          = "Required for accessing the storage account from the ai service and read/write data."
+#   scope                = module.storage_account.storage_account_id
+#   role_definition_name = "Storage Blob Data Contributor"
+#   principal_id         = module.user_assigned_identity.user_assigned_identity_principal_id
+#   principal_type       = "ServicePrincipal"
+# }
