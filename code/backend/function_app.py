c6044402ef4bd12510659977c137435d3d7e26eb
Marvin Buss <marvin.buss@gmail.com>
2024-10-22 16:51:17 +0200

    test with health

--- code/backend/function_app.py
+++ code/backend/function_app.py
@@ -1,8 +1,8 @@
 import azure.functions as func
 
-# from health.function import bp as bp_health
+from health.function import bp as bp_health
 from videoupload.function import bp as bp_videoupload
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 app.register_functions(bp_videoupload)
-# app.register_functions(bp_health)
+app.register_functions(bp_health)
