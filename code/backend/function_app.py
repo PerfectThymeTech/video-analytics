8e4e2c8e70549014126d820eaa2958b4f7047b67
Marvin Buss <marvin.buss@gmail.com>
2024-10-22 16:27:38 +0200

    Test other blob trigger

--- code/backend/function_app.py
+++ code/backend/function_app.py
@@ -2,7 +2,9 @@ import azure.functions as func
 
 # from health.function import bp as bp_health
 from videoupload.function import bp as bp_videoupload
+from blobtest.function import bp as bp_blobtest
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 app.register_functions(bp_videoupload)
+app.register_functions(bp_blobtest)
 # app.register_functions(bp_health)
