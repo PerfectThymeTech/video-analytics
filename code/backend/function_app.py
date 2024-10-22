8308211eee183cc55dc2c47b7537f3e3bf853118
Marvin Buss <marvin.buss@gmail.com>
2024-10-22 16:46:30 +0200

    Remove one trigger

--- code/backend/function_app.py
+++ code/backend/function_app.py
@@ -1,10 +1,8 @@
 import azure.functions as func
-from blobtest.function import bp as bp_blobtest
 
 # from health.function import bp as bp_health
 from videoupload.function import bp as bp_videoupload
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 app.register_functions(bp_videoupload)
-app.register_functions(bp_blobtest)
 # app.register_functions(bp_health)
