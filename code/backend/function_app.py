89b0e00d922a986437ed0bf32036ad1f37f6d824
Marvin Buss <marvin.buss@gmail.com>
2024-10-22 13:41:27 +0200

    Register second blueprint

--- code/backend/function_app.py
+++ code/backend/function_app.py
@@ -1,9 +1,8 @@
 import azure.functions as func
 
-# from videoupload.function import bp as bp_videoupload
+from videoupload.function import bp as bp_videoupload
 from health.function import bp as bp_health
-from models.health import HeartbeatResponse
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
-# app.register_functions(bp_videoupload)
+app.register_functions(bp_videoupload)
 app.register_functions(bp_health)
