1559b12ce5c7c4df5e2137bb742568c7a9a65107
Marvin Buss <marvin.buss@gmail.com>
2024-10-24 11:17:02 +0200

    Add logging for health endpoint

--- code/backend/health/function.py
+++ code/backend/health/function.py
@@ -1,3 +1,5 @@
+import logging
+
 import azure.functions as func
 from models.health import HeartbeatResponse
 
@@ -8,6 +10,7 @@ bp = func.Blueprint()
 @bp.route(route="v1/heartbeat")
 @bp.http_type(http_type=func.HttpMethod.GET)
 async def heartbeat(req: func.HttpRequest) -> func.HttpResponse:
+    logging.debug("Received Health Request.")
     response = HeartbeatResponse(is_alive=True).model_dump_json()
     return func.HttpResponse(
         body=response, status_code=200, mimetype="application/json"
