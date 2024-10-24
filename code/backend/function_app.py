b5a4881d64445ecd6c23eaf98a17fa11e6bf7cb8
Marvin Buss <marvin.buss@gmail.com>
2024-10-24 16:53:02 +0200

    lint

--- code/backend/function_app.py
+++ code/backend/function_app.py
@@ -1,7 +1,7 @@
 import azure.functions as func
+from aispeechanalysis.function import bp as bp_aispeech
 from health.function import bp as bp_health
 from videoupload.function import bp as bp_videoupload
-from aispeechanalysis.function import bp as bp_aispeech
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 app.register_functions(bp_health)
