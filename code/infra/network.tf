cff3641b070bc24c235efd7902e9463a26846809
Marvin Buss <marvin.buss@gmail.com>
2024-10-25 09:27:02 +0200

    Update to azapi v2

--- code/infra/network.tf
+++ code/infra/network.tf
@@ -3,7 +3,7 @@ resource "azapi_resource" "subnet_function" {
   name      = "WebAppSubnet"
   parent_id = data.azurerm_virtual_network.virtual_network.id
 
-  body = jsonencode({
+  body = {
     properties = {
       addressPrefix = var.subnet_cidr_function
       delegations = [
@@ -26,7 +26,7 @@ resource "azapi_resource" "subnet_function" {
       serviceEndpointPolicies = []
       serviceEndpoints        = []
     }
-  })
+  }
 }
 
 resource "azapi_resource" "subnet_private_endpoints" {
@@ -34,7 +34,7 @@ resource "azapi_resource" "subnet_private_endpoints" {
   name      = "PrivateEndpointSubnet"
   parent_id = data.azurerm_virtual_network.virtual_network.id
 
-  body = jsonencode({
+  body = {
     properties = {
       addressPrefix = var.subnet_cidr_private_endpoints
       delegations   = []
@@ -50,7 +50,7 @@ resource "azapi_resource" "subnet_private_endpoints" {
       serviceEndpointPolicies = []
       serviceEndpoints        = []
     }
-  })
+  }
 
   depends_on = [
     azapi_resource.subnet_function,
